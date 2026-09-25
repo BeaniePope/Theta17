7b16db0600953092d89d5faa0cfca6c8d0bdf7f3
BeaniePope <Swifter11110@gmail.com>
2026-09-24 22:59:49 -0400

    gay

---
+++ addons/factions/T17ChaosInsurgency.hpp
@@ -0,0 +1,226 @@
+class East 
+{ 
+    name = "$STR_A3_CfgGroups_East0"; 
+    side = 0; 
+
+    class T17_Zeus_Spacer 
+    {
+        name = "[Theta - 17] Chaos Insurgency"; 
+
+        // Rebel Alliance
+        class T17_ChaosInsurgency_Groups
+        { 
+            dlc = "AWR"; 
+            name = "Infantry"; 
+            class 80_RA_Sentry 
+            { 
+                name = "[80th] Sentry"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {5,-5,0}; 
+                }; 
+            }; 
+            class 80_RA_RifleSquad 
+            { 
+                name = "[80th] Rifle Squad"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_SquadLeader"; 
+                    rank = "SERGEANT"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {5,-5,0}; 
+                }; 
+                class Unit2 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_RTO"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-5,-5,0}; 
+                }; 
+                class Unit3 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {10,-5,0}; 
+                }; 
+                class Unit4 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_AT"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-10,-5,0}; 
+                }; 
+                class Unit5 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {15,-5,0}; 
+                }; 
+                class Unit6 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-15,-5,0}; 
+                }; 
+                class Unit7 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_MG"; 
+                    rank = "PRIVATE"; 
+                    position[] = {20,-5,0}; 
+                }; 
+                class Unit8 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {-20,-5,0}; 
+                }; 
+                class Unit9 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Medic"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-25,-5,0}; 
+                }; 
+            }; 
+            class 80_RA_Fireteam 
+            { 
+                name = "[80th] Fireteam"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {5,-5,0}; 
+                }; 
+                class Unit2 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-5,-5,0}; 
+                }; 
+                class Unit3 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Medic"; 
+                    rank = "PRIVATE"; 
+                    position[] = {10,-5,0}; 
+                }; 
+            }; 
+            class 80_RA_MachineGunTeam 
+            { 
+                name = "[80th] Machine Gun Team"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_MG"; 
+                    rank = "PRIVATE"; 
+                    position[] = {5,-5,0}; 
+                }; 
+                class Unit2 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "PRIVATE"; 
+                    position[] = {-5,-5,0}; 
+                }; 
+            };
+            class 80_RA_AntiTank 
+            { 
+                name = "[80th] Anti-Tank Team"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_AT"; 
+                    rank = "PRIVATE"; 
+                    position[] = {5,-5,0}; 
+                }; 
+            }; 
+            class 80_RA_Sniper 
+            { 
+                name = "[80th] Sniper Team"; 
+                side = 0; 
+                faction = "AWR_Rebel_FactionClass"; 
+                rarityGroup = 0.3; 
+                dlc = "AWR"; 
+                class Unit0 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Sniper"; 
+                    rank = "SERGEANT"; 
+                    position[] = {0,0,0}; 
+                }; 
+                class Unit1 
+                { 
+                    side = 0; 
+                    vehicle = "AWR_Rebel_Rifleman"; 
+                    rank = "CORPORAL"; 
+                    position[] = {5,-5,0}; 
+                }; 
+            }; 
+        };
+    }; 
+};
--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,3 +1,4 @@
+#include "includes.hpp"
 class CfgPatches 
 {
     class T17_Main
@@ -21,6 +22,33 @@ class CfgPatches
     };
 };
 
+
+class CfgEditorCategories
+{
+	class T17_Theta17
+    {   
+        displayName = "Theta - 17";
+        scopeCurator = 2;
+        scopeEditor = 2;
+    };
+    class T17_ChaosInsurgency
+    {
+        displayName = "[T17] Chaos Insurgency";
+        scopeCurator = 2;
+        scopeEditor = 2;
+    };
+};
+
+class CfgEditorSubCategories
+{
+	class T17_MobileTaskForce
+	{
+		displayName = "Mobile Task Force"; 
+        scopeCurator = 2;
+        scopeeditor = 2;
+	};
+};
+
 class CfgWeapons
 {
     class U_B_CombatUniform_mcam_vest;
@@ -33,7 +61,7 @@ class CfgWeapons
         class ItemInfo: UniformItem
 		{
 			uniformModel="-";
-			uniformClass="T17_CombatShirt_Uniform";
+			uniformClass="T17_Unit";
 			containerClass="Supply40";
 			mass=40;
 		};
@@ -42,16 +70,59 @@ class CfgWeapons
 
 class CfgVehicles
 {
+
+	// Backpacks
+	class MTF_FCPC_117G_veil;
+	class MTF_FCPC_Satcom_veil;
+	class BTRG_ZIPON_LR_BLK;
+
+	class T17_LR_Veil: MTF_FCPC_117G_veil
+	{
+		displayName = "[Theta - 17] AN/PRC 117G Veil";
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_radio = 1;
+		tf_dialog = "anarc210_radio_dialog";
+		tf_subtype = "digital_lr";
+		tf_dialogUpdate = "[""CH%1""] call TFAR_fnc_updateLRDialogToChannel;";
+		tf_range = 5000;
+	};
+	class T17_LR_Satcom:  MTF_FCPC_Satcom_veil
+	{
+		displayName = "[Theta - 17] AN/PRC 117G Veil (SATCOM)";
+		tf_radio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "anarc210_radio_dialog";
+		tf_subtype = "digital_lr";
+		tf_dialogUpdate = "[""CH%1""] call TFAR_fnc_updateLRDialogToChannel;";
+		tf_range = 5000;
+	};
+	class T17_LR_Backpack: BTRG_ZIPON_LR_BLK
+	{
+		displayName = "[Theta - 17] Backpack (Radio)";
+		tf_radio = 1;
+		tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "anarc210_radio_dialog";
+		tf_subtype = "digital_lr";
+		tf_dialogUpdate = "[""CH%1""] call TFAR_fnc_updateLRDialogToChannel;";
+		tf_range = 2000;
+	};
+
+
+
+	// Units
     class B_Soldier_F;
-    class T17_CombatShirt_Uniform: B_Soldier_F
+    class T17_Unit: B_Soldier_F
     {
         author = "Queen";
-        displayName = "[Theta - 17] Combat Shirt w/ Sleeves";
+        displayName = "Operator (Rifleman)";
         uniformModel = "T17_CombatShirt_Item";
-		scope=2;
-		identityTypes[]={};
-		model="\A3\characters_F\BLUFOR\b_soldier_01.p3d";
-		uniformClass="T17_CombatShirt_Item";
+		scope = 2;
+		identityTypes[] = {};
+		model = "\A3\characters_F\BLUFOR\b_soldier_01.p3d";
+		uniformClass = "T17_CombatShirt_Item";
+		side = 2;
+		editorCategory = "T17_Theta17";
+		editorSubCategory = "T17_MobileTaskForce";
 		hiddenSelections[] = 
         {
             "camo",
@@ -64,11 +135,13 @@ class CfgVehicles
         };
 		weapons[]=
 		{
+			"hlc_wp_C7"
 			"Throw",
 			"Put"
 		};
 		respawnWeapons[]=
 		{
+			"hlc_wp_C7"
 			"Throw",
 			"Put"
 		};
@@ -80,6 +153,8 @@ class CfgVehicles
 			"SmokeShellGreen",
 			"Chemlight_green",
 			"Chemlight_green"
+			MAG_5("hlc_30rnd_556x45_EPR")
+
 		};
 		respawnMagazines[]=
 		{
@@ -88,7 +163,8 @@ class CfgVehicles
 			"SmokeShell",
 			"SmokeShellGreen",
 			"Chemlight_green",
-			"Chemlight_green"
+			"Chemlight_green",
+			MAG_5("hlc_30rnd_556x45_EPR")
 		};
 		linkedItems[]=
 		{
